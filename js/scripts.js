05c3ffc9a6db8dd0c004badc1bf833563cdd4b0e
Hamake D. Arayanai <45520325+hamakedarayanai@users.noreply.github.com>
2025-02-12 22:23:41 +0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,40 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const whatsappForm = document.getElementById('whatsappForm');
-    if (whatsappForm) {
-        whatsappForm.addEventListener('submit', function(event) {
-            event.preventDefault();
-            let phone = document.getElementById('phone');
-            let message = document.getElementById('message').value.trim();
-            if (!phone.checkValidity()) {
-                phone.classList.add("is-invalid");
-                return;
-            }
-            let whatsappURL = `https://wa.me/${phone.value.trim()}`;
-            if (message) {
-                whatsappURL += `?text=${encodeURIComponent(message)}`;
-            }
-            window.open(whatsappURL, '_blank');
-        });
-    }
+  // WhatsApp Form Handling
+  const whatsappForm = document.getElementById('whatsappForm');
+  if (whatsappForm) {
+    whatsappForm.addEventListener('submit', function(event) {
+      event.preventDefault();
+
+      const phoneInput = document.getElementById('phone');
+      const messageInput = document.getElementById('message');
+
+      const phone = phoneInput.value.trim();
+      const message = messageInput.value.trim();
+
+      if (!isValidPhoneNumber(phone)) {
+        phoneInput.classList.add("is-invalid");
+        return;
+      } else {
+        phoneInput.classList.remove("is-invalid");
+      }
+
+      let whatsappURL = `https://wa.me/${phone}`;
+      if (message) {
+        whatsappURL += `?text=${encodeURIComponent(message)}`;
+      }
+      window.open(whatsappURL, '_blank');
+    });
+  }
+
+  function isValidPhoneNumber(phoneNumber) {
+    const phoneRegex = /^\d{8,}$/; // Customize this regex!
+    return phoneRegex.test(phoneNumber);
+  }
+
+
+  // Current Year in Footer
+  const currentYearSpan = document.getElementById("currentYear");
+  if (currentYearSpan) {
+    currentYearSpan.textContent = new Date().getFullYear();
+  }
 });
